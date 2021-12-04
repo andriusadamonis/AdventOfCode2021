6bd9f7a81122234ddd8d99b6021117dee88c03ae
Andrius Adamonis <andrius.adamonis@tietoevry.com>
2021-12-04 06:48:45 +0200

    Day2 part 1 - add a bit of error handling to get taste of it

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -9,15 +9,22 @@ class Day2 {
 
     }
 
-    private fun calculatePosition(instructions: Iterable<String>): Pos {
+    fun calculatePosition(instructions: Iterable<String>): Pos {
         var result: Pos = Pos(0, 0)
+        var no = 0
         instructions.forEach() {
+            no++
             val instruction = it.split(" ")
             if (instruction.size >= 2) {
-                when (instruction[0]) {
-                    "forward" -> result.horizontal += instruction[1].toInt()
-                    "down" -> result.depth += instruction[1].toInt()
-                    "up" -> result.depth -= instruction[1].toInt()
+                try {
+                    when (instruction[0]) {
+                        "forward" -> result.horizontal += instruction[1].toInt()
+                        "down" -> result.depth += instruction[1].toInt()
+                        "up" -> result.depth -= instruction[1].toInt()
+                        else -> throw NotImplementedError("Error in command #${no}: Not implemented: \"${instruction[0]}\"")
+                    }
+                } catch (e: NumberFormatException) {
+                    throw NumberFormatException("Error in command #${no}: ${e.message}")
                 }
             }
         }
--- src/test/kotlin/Day2Test.kt
+++ src/test/kotlin/Day2Test.kt
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.test.assertFailsWith
 
 internal class Day2Test {
 
@@ -9,6 +10,24 @@ internal class Day2Test {
         val actual = Day2().positionMultiplied(readLines("day2.txt"))
 
         assertEquals(150, actual)
+
+    }
+
+    @Test
+    fun testPositionMultipliedCommandError() {
+
+        val input = listOf("blabla 1", "down 1")
+        val exception = assertFailsWith<NotImplementedError>() { Day2().calculatePosition(input) }
+        assertEquals("Error in command #1: Not implemented: \"blabla\"", exception.message)
+
+    }
+
+    @Test
+    fun testPositionMultipliedParseError() {
+
+        val input = listOf("forward 1", "down xxx")
+        val exception = assertFailsWith<NumberFormatException>() { Day2().calculatePosition(input) }
+        assertEquals("Error in command #2: For input string: \"xxx\"", exception.message)
 
     }
 
